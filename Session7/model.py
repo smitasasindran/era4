7e89dabd968d4bafb3dc0629d8f80eb965b6e9d4
Smita Sasindran <smita@smarthomesentry.com>
2025-10-04 07:09:45 +0530

    Modularizing the code from iteration4 - 85% acc, ~100K params, no albumentations

---
+++ Session7/model.py
@@ -0,0 +1,200 @@
+# import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+dropout_value = 0.1
+
+
+#https://github.com/seungjunlee96/Depthwise-Separable-Convolution_Pytorch
+def depthwise_separable_layer(nin, nout, dropout_value, padding=0):
+    depthwise = nn.Conv2d(nin, nin, kernel_size=3, padding=padding, groups=nin, bias=False)
+    pointwise = nn.Conv2d(nin, nout, kernel_size=1, bias=False)
+
+    depthwise_separable = nn.Sequential(
+        depthwise,
+        nn.ReLU(),
+        nn.BatchNorm2d(nin),
+        nn.Dropout(dropout_value),
+        pointwise,
+        nn.ReLU(),
+        nn.BatchNorm2d(nout),
+        nn.Dropout(dropout_value)
+    )
+    return depthwise_separable
+
+
+class Net(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+
+        # Conv Block 1
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=16, kernel_size=(3, 3), padding=1, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(16),
+            nn.Dropout(dropout_value)
+        ) # output_size = 32, rf=3
+
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(3, 3), padding=1, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.Dropout(dropout_value)
+        ) # output_size = 32, rf=5
+
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3), padding=1, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.Dropout(dropout_value)
+        ) # output_size = 32, rf=7
+
+        # transition 1 - use stride2 instead of MP
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3), padding=0, stride=2, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Dropout(dropout_value)
+        ) # output_size = 15, rf=9
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=16, kernel_size=(1, 1), padding=0, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(16),
+            nn.Dropout(dropout_value)
+        ) # output_size = 15, rf=9
+
+        # Conv Block 2 -------------------
+        self.conv6 = nn.Sequential( # padding
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(3, 3), padding=1, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.Dropout(dropout_value)
+        ) # output_size = 15, rf=13 (jump2)
+
+        self.conv7 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3), padding=1, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.Dropout(dropout_value)
+        ) # output_size = 15, rf=17
+
+        self.conv8 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3), padding=1, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Dropout(dropout_value)
+        ) # output_size = 15, rf=21
+
+        # Transition 2 --
+        self.conv9 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=16, kernel_size=(1, 1), padding=0, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(16),
+            nn.Dropout(dropout_value)
+        ) # output_size = 15, rf=21
+
+
+        # Conv Block 3 ---------------------
+        self.conv10 = nn.Sequential( # padding
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(3, 3), padding=1, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.Dropout(dropout_value)
+        ) # output_size = 15, rf=25
+
+        self.conv11 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3), padding=0, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.Dropout(dropout_value)
+        ) # output_size = 13, rf=29
+
+        # self.conv12 = nn.Sequential(
+        #     nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3), padding=0, bias=False),
+        #     nn.ReLU(),
+        #     nn.BatchNorm2d(32),
+        #     nn.Dropout(dropout_value)
+        # ) # output_size = 11, rf=33
+        self.conv12 = depthwise_separable_layer(32, 64, dropout_value, padding=0) # output_size=11, rf=33
+
+        # transition 3
+        self.conv13 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=16, kernel_size=(1, 1), padding=0, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(16),
+            nn.Dropout(dropout_value)
+        ) # output_size = 11, rf=33
+
+        # Conv Block 4 - with dilation
+        self.conv14 = nn.Sequential( # Dilation
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(3, 3), padding=0, dilation=2, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.Dropout(dropout_value)
+        ) # output_size = 7, rf=41 # dilation
+
+        self.conv15 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3), padding=0, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.Dropout(dropout_value)
+        ) # output_size = 5, rf=45
+
+        # self.conv16 = nn.Sequential(
+        #     nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3), padding=0, bias=False),
+        #     nn.ReLU(),
+        #     nn.BatchNorm2d(64),
+        #     nn.Dropout(dropout_value)
+        # ) # output_size = 6
+
+
+        # Output
+        self.gap = nn.Sequential(
+            nn.AvgPool2d(kernel_size=5)
+        ) # output_size = 5
+
+        self.conv17 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=10, kernel_size=(1, 1), padding=0, bias=False),
+            # nn.BatchNorm2d(10),
+            # nn.Dropout(dropout_value)
+            # nn.ReLU()
+        ) # output_size = 10, rf=45
+
+
+
+    def forward(self, x):
+        # block1
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x) # stride 2
+        # print(f"conv4 out: {x.shape}")
+
+        # block2
+        x = self.conv5(x)
+        x = self.conv6(x)
+        x = self.conv7(x)
+        x = self.conv8(x)
+
+        # block3
+        x = self.conv9(x)
+        x = self.conv10(x)
+        x = self.conv11(x)
+        x = self.conv12(x)
+
+        # block4
+        x = self.conv13(x)
+        x = self.conv14(x)
+        x = self.conv15(x)
+        # x = self.conv16(x)
+        # print(f"conv15 out: {x.shape}")
+
+        # Output
+        x = self.gap(x)
+        x = self.conv17(x)
+        x = x.view(-1, 10)
+
+        return F.log_softmax(x, dim=-1)
+
---
+++ Session7/utils.py
@@ -0,0 +1,135 @@
+import torch
+# import torch.nn as nn
+import torch.nn.functional as F
+# import torch.optim as optim
+# import torchvision
+from torchvision import datasets, transforms
+# import numpy as np
+import albumentations as A
+from torchsummary import summary
+
+from tqdm import tqdm
+
+
+def init_setup():
+    SEED = 1
+
+    # CUDA?
+    cuda = torch.cuda.is_available()
+    print("CUDA Available?", cuda)
+
+    # For reproducibility
+    torch.manual_seed(SEED)
+
+    if cuda:
+        torch.cuda.manual_seed(SEED)
+    return cuda
+
+
+def create_transformations():
+    # Train Phase transformations
+    train_transforms = transforms.Compose([
+        # A.HorizontalFlip(p=0.5),
+        transforms.RandomHorizontalFlip(p=0.5),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=(0.4914, 0.4822, 0.4465), std=(0.2470, 0.2435, 0.2616))
+    ])
+
+    # Test Phase transformations
+    test_transforms = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=(0.4914, 0.4822, 0.4465), std=(0.2470, 0.2435, 0.2616))
+    ])
+
+    return train_transforms, test_transforms
+
+
+def load_dataset(train_transforms, test_transforms):
+    train = datasets.CIFAR10('./data', train=True, download=True, transform=train_transforms)
+    test = datasets.CIFAR10('./data', train=False, download=True, transform=test_transforms)
+    return train, test
+
+
+def get_dataloaders(train, test, cuda, batch_size=128):
+    # batch_size = 512  # 256
+    cpu_batch_size = 128  # 64
+    # dataloader arguments - something you'll fetch these from cmdprmt
+    dataloader_args = dict(shuffle=True, batch_size=batch_size, num_workers=4, pin_memory=True) if cuda else dict(
+        shuffle=True, batch_size=cpu_batch_size)
+
+    # train dataloader
+    train_loader = torch.utils.data.DataLoader(train, **dataloader_args)
+
+    # test dataloader
+    test_loader = torch.utils.data.DataLoader(test, **dataloader_args)
+
+    return train_loader, test_loader
+
+
+def model_summary(model):
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+    # print(device)
+    model = model.to(device)
+    summary(model, input_size=(3, 32, 32))
+
+
+def train(model, device, train_loader, optimizer, epoch, train_losses, train_acc):
+    model.train()
+    pbar = tqdm(train_loader)
+    correct = 0
+    processed = 0
+    for batch_idx, (data, target) in enumerate(pbar):
+        # get samples
+        data, target = data.to(device), target.to(device)
+
+        # Init
+        optimizer.zero_grad()
+        # In PyTorch, we need to set the gradients to zero before starting to do backpropragation because PyTorch accumulates the gradients on subsequent backward passes.
+        # Because of this, when you start your training loop, ideally you should zero out the gradients so that you do the parameter update correctly.
+
+        # Predict
+        y_pred = model(data)
+
+        # Calculate loss
+        loss = F.nll_loss(y_pred, target)
+        train_losses.append(loss)
+
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+
+        # Update pbar-tqdm
+
+        pred = y_pred.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+        correct += pred.eq(target.view_as(pred)).sum().item()
+        processed += len(data)
+
+        pbar.set_description(desc= f'Loss={loss.item()} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}')
+        train_acc.append(100*correct/processed)
+
+    return train_losses, train_acc
+
+
+def test(model, device, test_loader, test_losses, test_acc):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+    test_losses.append(test_loss)
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
+    test_acc.append(100. * correct / len(test_loader.dataset))
+
+    return test_losses, test_acc
